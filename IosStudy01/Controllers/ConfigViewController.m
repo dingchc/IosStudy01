cac412428c2973515c567c308e1ce34127407103
dingchongci <dingchongci@aspirecn.com>
2018-01-22 18:36:45 +0800

    update

--- IosStudy01/Controllers/ConfigViewController.m
+++ IosStudy01/Controllers/ConfigViewController.m
@@ -10,6 +10,11 @@
 #import "ConfigEntry.h"
 #import "ConfigTableViewCell.h"
 
+typedef NS_ENUM(NSUInteger, Mode){
+    ModeMedium = 1,
+    ModeLarge
+};
+
 @interface ConfigViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property(nonatomic, strong) UITableView* tableView;
@@ -46,25 +51,25 @@
 -(void) initData {
     
     // avatar
-    ConfigEntry* avatarEntry = [ConfigEntry entryWithTitle:@"我的信息" withIcon:@"avatar_default_big.jpg" withMode : 2];
+    ConfigEntry* avatarEntry = [ConfigEntry entryWithTitle:@"我的信息" withIcon:@"avatar_default_big.jpg" withMode : ModeLarge];
     NSArray<ConfigEntry*>* avatarArray = [[NSArray alloc] initWithObjects:avatarEntry, nil];
     [self.itemDictionary  setValue:avatarArray forKey:self.itemArray[0]];
     
     // homepage
-    ConfigEntry* homePageEntry = [ConfigEntry entryWithTitle:@"我的主页" withIcon:@"my_page_btn_bg" withMode : 1];
-    ConfigEntry* favorEntry = [ConfigEntry entryWithTitle:@"我的收藏" withIcon:@"my_favorite_bg" withMode : 1];
-    ConfigEntry* creditEntry = [ConfigEntry entryWithTitle:@"我的积分" withIcon:@"my_credits_bg" withMode : 1];
+    ConfigEntry* homePageEntry = [ConfigEntry entryWithTitle:@"我的主页" withIcon:@"my_page_btn_bg" withMode : ModeMedium];
+    ConfigEntry* favorEntry = [ConfigEntry entryWithTitle:@"我的收藏" withIcon:@"my_favorite_bg" withMode : ModeMedium];
+    ConfigEntry* creditEntry = [ConfigEntry entryWithTitle:@"我的积分" withIcon:@"my_credits_bg" withMode : ModeMedium];
     NSArray<ConfigEntry*>* homePageArray = [[NSArray alloc] initWithObjects:homePageEntry, favorEntry, creditEntry, nil];
 
     [self.itemDictionary  setValue:homePageArray forKey:self.itemArray[1]];
 
     // class
-    ConfigEntry* classPageEntry = [ConfigEntry entryWithTitle:@"我的班级" withIcon:@"my_favorite_bg" withMode : 1];
+    ConfigEntry* classPageEntry = [ConfigEntry entryWithTitle:@"我的班级" withIcon:@"my_favorite_bg" withMode : ModeMedium];
     NSArray<ConfigEntry*>* classPageArray = [[NSArray alloc] initWithObjects:classPageEntry, nil];
     [self.itemDictionary  setValue:classPageArray forKey:self.itemArray[2]];
 
     // config
-    ConfigEntry* configEntry = [ConfigEntry entryWithTitle:@"设置" withIcon:@"my_setting_bg" withMode : 1];
+    ConfigEntry* configEntry = [ConfigEntry entryWithTitle:@"设置" withIcon:@"my_setting_bg" withMode : ModeMedium];
     NSArray<ConfigEntry*>* configArray = [[NSArray alloc] initWithObjects:configEntry, nil];
     [self.itemDictionary  setValue:configArray forKey:self.itemArray[3]];
 }
@@ -130,7 +135,7 @@
     cell.titleLabel.text = dataArray[indexPath.row].title;
     cell.iconImageView.image = [UIImage imageNamed:entry.iconName];
     
-    if (entry.itemMode == 2) {
+    if (entry.itemMode == ModeLarge) {
         [cell remake];
     }
     
@@ -143,7 +148,7 @@
     NSArray<ConfigEntry*>* dataArray = [self.itemDictionary objectForKey:key];
     ConfigEntry* entry = [self getEntryAtIndexPath:indexPath withDataArray: dataArray];
     
-    return entry.itemMode == 2 ? 80 : 48;
+    return entry.itemMode == ModeLarge ? 80 : 48;
 }
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
