a33694936a3c6d75809845c684e805e8af531187
dingchongci <dingchongci@aspirecn.com>
2018-05-22 23:02:40 +0800

    update

--- IosStudy01/Controllers/Pages/DrawerViewController.m
+++ IosStudy01/Controllers/Pages/DrawerViewController.m
@@ -20,19 +20,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString* dbPath = [paths.firstObject stringByAppendingPathComponent:@"1.sqlite"];
+    // 初始化数据库
+    [self initDb];
     
-    FMDatabase* db = [FMDatabase databaseWithPath:dbPath];
-    [db open];
-    
-    [db executeUpdate:@"create table if not "];
-    
-    FMResultSet* set = [db executeQuery:@""];
-    while ([set next]) {
-        [set stringForColumn:@"name"];
-    }
-    
+    // 请求
+//    [self request];
+    [self doPost];
     
     self.view.backgroundColor = [UIColor whiteColor];
 
@@ -44,6 +37,91 @@
     [self parseJson];
 }
 
+/**
+ * 请求
+ */
+- (void) request {
+    
+    NSString *url = @"https://www.apiopen.top/novelApi";
+    
+    NSURL *encodeUrl = [NSURL URLWithString:[url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:encodeUrl];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSLog(@"请求完成...");
+        if (!error) {
+            NSDictionary *resDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            id code = resDict[@"code"];
+            NSString *msg = resDict[@"msg"];
+            
+            NSLog(@"code=%@ msg=%@", code, msg);
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self processRequest:resDict];
+            });
+        }
+    }];
+    
+    [task resume];
+}
+
+- (void) processRequest:(NSDictionary*)res {
+    
+    NSArray *array = [res objectForKey:@"data"];
+    
+    for (NSDictionary* dict in array) {
+        NSLog(@"bookName = %@", dict[@"bookname"]);
+    }
+}
+
+- (void) doPost {
+    
+    NSString *url = @"https://www.apiopen.top/satinGodApi";
+    NSURL *encodeUrl = [NSURL URLWithString:url];
+    NSString *post = [NSString stringWithFormat:@"type=%@&page=%@", @1, @1];
+    
+    NSData *data = [post dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:encodeUrl];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:data];
+    
+    NSURLSessionConfiguration *defaultConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:defaultConfig delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    
+    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        NSLog(@"请求完成...msg=%@", dict[@"msg"]);
+    }];
+    
+    [task resume];
+}
+
+- (void) initDb {
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString* dbPath = [paths.firstObject stringByAppendingPathComponent:@"1.sqlite"];
+    
+    FMDatabase* db = [FMDatabase databaseWithPath:dbPath];
+    [db open];
+    
+    [db executeUpdate:@"create table if not exists TABLE_STU(id integer primary key, name text, age integer)  "];
+    
+    [db executeUpdate:@"insert into TABLE_STU values(1, '甲', 12)"];
+    [db executeUpdate:@"insert into TABLE_STU values(2, '乙', 13)"];
+    
+    FMResultSet* set = [db executeQuery:@"select * from TABLE_STU"];
+    
+    while ([set next]) {
+        
+        NSString* name = [set stringForColumn:@"name"];
+        int age = [set intForColumn:@"age"];
+        
+        NSLog(@"name=%@, age=%d", name, age);
+    }
+}
 
 - (void) parseJson {
     
